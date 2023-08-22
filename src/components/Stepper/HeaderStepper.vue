6b339abf2cde2ca9d2fe26cae1169c00a20abad7
DkG-07 <dkg.0717@gmail.com>
2023-08-22 13:06:17 -0600

    Avances en header de stepper

--- src/components/Stepper/HeaderStepper.vue
+++ src/components/Stepper/HeaderStepper.vue
@@ -2,8 +2,10 @@
   <section class="header-stepper">
     <p>Firmar nuevo documento</p>
     <ul class="stepper-list">
-      <li v-for="(option, index) in stepperOptions">
-        <p>{{ index + 1 }}</p>
+      <li v-for="(option, idx) in stepperOptions">
+        <p class="step-circle" :class="{ 'step-active': index == idx }">
+          {{ idx + 1 }}
+        </p>
         <p>{{ option }}</p>
       </li>
     </ul>
@@ -11,6 +13,12 @@
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia';
+import { useStepperStore } from '@/stores/stepper';
+
+const stepperStore = useStepperStore()
+const { index } = storeToRefs(stepperStore)
+
 const stepperOptions = [
   "Cargar documento",
   "Indicar firmantes",
