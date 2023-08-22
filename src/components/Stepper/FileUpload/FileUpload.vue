674dcb799608bfd5c4da81a99ebda9c7926578b4
DkG-07 <dkg.0717@gmail.com>
2023-08-22 12:45:03 -0600

    Se agregó pinia

--- src/components/Stepper/FileUpload/FileUpload.vue
+++ src/components/Stepper/FileUpload/FileUpload.vue
@@ -1,8 +1,32 @@
 <template>
-  <section class="fileupload_container">
+  <section class="fileupload_container" @dragover="filesSaved('drag', $event)">
     <p>Arrastra y suelta tus documentos aquí o Buscar Archivo.</p>
+    <input class="file" type="file" id="files" name="files" accept="application/pdf" @change="filesSaved('input', $event)"
+      multiple>
+    <ul v-if="files.length > 0">
+      <li v-for="(file, index) in files" @click="removeFile(index)">
+        {{ file.name }}
+      </li>
+    </ul>
+    <button>Siguiente</button>
   </section>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+let files = ref([])
+
+const filesSaved = (evtType, evt) => {
+  evt.preventDefault();
+  files.value = (evtType == 'drag') ? [...evt.dataTransfer.files] : [...evt.target.files]
+}
+
+const removeFile = (index) => {
+  files.value.splice(index, 1)
+}
+
+</script>
 
 <style>
 @import url('../../../assets/css/fileupload.css');
---
+++ src/stores/stepper.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia';
+
+export const useStepperStore = defineStore('stepper', {
+
+  state: () => ({
+    index: 0
+  }),
+
+  actions: {
+
+  }
+
+})
