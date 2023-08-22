56fc998b31b5b9642fbbc0143793f98a8db201c2
DkG-07 <dkg.0717@gmail.com>
2023-08-22 09:25:43 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header></Header>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script setup>
+import Header from './components/Header/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <section>
+      <img src="" alt="">
+    </section>
+    <section>
+      <p></p>
+      <img src="" alt="">
+    </section>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+@import url('../../assets/css/header/header.css');
+</style>
