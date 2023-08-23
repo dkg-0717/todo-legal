8538dbfe1a33157c6b7cfbc15fa7d90465643886
DkG-07 <dkg.0717@gmail.com>
2023-08-22 18:31:38 -0600

    Ajustes de css y seccion como funciona

--- src/components/Stepper/steps/StepOne.vue
+++ src/components/Stepper/steps/StepOne.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="step-1">
-    <section class="section">
+    <section class="section section-1">
       <h1 class="txt-how-works txt-center">¿Cómo funciona?</h1>
-      <img src="" alt="">
+      <img class="desktop-image" src="../../../assets/images/desktop.png" alt="Imagen de una mujer en una pc">
       <p class="txt-free-service txt-center">Este es un servicio gratuito de <span class="txt-blue">todolegal</span></p>
     </section>
     <section class="section">
@@ -41,6 +41,18 @@ console.log(index.value)
   border-radius: 8px;
 }
 
+.section-1 {
+  padding: 44px 58px;
+}
+
+.desktop-image {
+  max-width: 700px;
+  width: 100%;
+  max-height: 378px;
+  object-fit: contain;
+  margin-block-start: 125px;
+}
+
 .txt-upload-docs {
   font-size: 2.4rem;
   font-weight: bold;
@@ -60,6 +72,7 @@ console.log(index.value)
 
 .txt-free-service {
   font-size: 2.8rem;
+  margin-block-start: 55px;
   color: var(--minusLightGray);
 }
 
